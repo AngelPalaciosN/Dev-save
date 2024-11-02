ac27cf4277836cc0d8ca4adebcee7847d66af5d1
AngelPalaciosN <angeld_palaciosn@soy.sena.edu.co>
2024-11-02 09:28:58 -0500

    Ejercicio matriz
    Se termino una priemra version del ejecicio matriz

--- js/Arrays/Ejercicio8/app.js
+++ js/Arrays/Ejercicio8/app.js
@@ -10,11 +10,17 @@ const rl = readline.createInterface({
 
 let nums = [];
 
-let matriz = [[nums]];
+let matriz = [];
 
-let min = matriz[0][0];
-let max = matriz[0][0];
+let min,max;
 
+
+
+function restart(){
+    this.res = function(){
+        new matriz_max().ingresar();
+    }
+}
 
 
 //Funcion principal
@@ -22,17 +28,29 @@ function matriz_max(){
     this.ingresar = function(){
         rl.question('Que numero desea añadir a la matriz? ', (answer) => {
             let num = parseInt(answer);
-            nums.push(num);
+            let fila = answer.split(',').map(num => parseInt(num.trim()));
+            if (isNaN(num)){
+                console.log('Porfavor, Ingrese un numero correcto')
+                new restart().res();
+            }else{
+            matriz.push(fila);
             rl.question('Desea ingresar otro numero? (s/n) ', (response) => {
                 if (response === 's') {
-                    this.ingresar();
-                } else {
+                   new restart().res();
+                } else if (response === 'n'){
+                    min = Math.min(...matriz.flat());
+                    max = Math.max(...matriz.flat());
                     console.table(matriz);
+                    console.log();
+                    console.log(`El numero menor es ${min} y el mayor es ${max}`)
+                    rl.close();
+                }else{
+                    console.log('Elija una opcion correcta')
+                    new restart().res();
                 }
-                rl.close();
-            });
-        });
-    }
+                });
+            }});
+        }
 }
 
 
